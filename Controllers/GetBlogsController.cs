3fa2cbd380091d4c81d76366d2b82146b6089697
AbhishekSunar01 <a8hishek01@gmail.com>
2024-05-15 22:35:28 +0545

    one last ride

--- Controllers/GetBlogsController.cs
+++ Controllers/GetBlogsController.cs
@@ -1,61 +1,89 @@
-﻿/*using BisleriumServer.Data;
-using BisleriumServer.Model.DTO;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
+﻿
+// using Microsoft.AspNetCore.Mvc;
+// using Microsoft.EntityFrameworkCore;
+// using BisleriumServer.Data;
+// using BisleriumServer.Model;
 
-namespace BisleriumServer.Controllers
-{
+// namespace BisleriumServer.Controllers
+// {
+// 	[ApiController]
+// 	[Route("api/[controller]")]
+// 	public class GetBlogsController : ControllerBase
+// 	{
+// 		private readonly ApplicationDbContext _context;
 
-    [ApiController]
-    [Route("api/[controller]")]
-    public class GetBlogsController : Controller
-    {
-        private readonly ApplicationDbContext _context;
+// 		public GetBlogsController(ApplicationDbContext context)
+// 		{
+// 			_context = context;
+// 		}
 
-        public GetBlogsController(ApplicationDbContext context)
-        {
-            _context = context;
-        }
+// 		// Get random blogs
+// 		[HttpGet("random")]
+// 		public async Task<IActionResult> GetRandomBlogs()
+// 		{
+// 			var blogs = await _context.Blogs
+// 				.OrderBy(b => Guid.NewGuid())
+// 				.Take(10)
+// 				.Include(b => b.User)
+// 				.Include(b => b.Comments)
+// 				.ToListAsync();
 
-		// GET: api/blog
-		[HttpGet]
-		public async Task<IActionResult> GetAllBlogs()
-		{
-			var blogs = await _context.Blogs
-				.Include(b => b.User)
-				.Include(b => b.Comments) // Make sure to include comments
-				.ToListAsync();
+// 			return Ok(blogs.Select(b => MapBlogToDto(b)));
+// 		}
 
-			if (blogs == null || !blogs.Any())
-			{
-				return NotFound("No blogs found.");
-			}
+// 		// Get blogs by recency
+// 		[HttpGet("recent")]
+// 		public async Task<IActionResult> GetRecentBlogs()
+// 		{
+// 			var blogs = await _context.Blogs
+// 				.OrderByDescending(b => b.CreatedAt)
+// 				.Take(10)
+// 				.Include(b => b.User)
+// 				.Include(b => b.Comments)
+// 				.ToListAsync();
 
-			var blogDTOs = blogs.Select(b => new BlogDTO
-			{
-				Id = b.Id,
-				Title = b.Title,
-				Description = b.Description,
-				CreatedAt = b.CreatedAt,
-				Username = b.User.Username,
-				LikesCount = b.LikesCount,
-				DislikesCount = b.DislikesCount,
-				CommentsCount = b.Comments.Count, // Calculate the count of comments
-				Comments = b.Comments.Select(c => new SimpleCommentDto
-				{
-					Id = c.Id,
-					Text = c.Text
-				}).ToList() // Map comments if necessary
-			}).ToList();
+// 			return Ok(blogs.Select(b => MapBlogToDto(b)));
+// 		}
 
-			return Ok(blogDTOs);
-		}
+// 		// Get blogs by popularity (likes and comments)
+// 		[HttpGet("popular")]
+// 		public async Task<IActionResult> GetPopularBlogs()
+// 		{
+// 			var blogs = await _context.Blogs
+// 				.Include(b => b.User)
+// 				.Include(b => b.Comments)
+// 				.OrderByDescending(b => b.Likes.Count + b.Comments.Count)
+// 				.Take(10)
+// 				.ToListAsync();
 
+// 			return Ok(blogs.Select(b => MapBlogToDto(b)));
+// 		}
 
+// 		private dynamic MapBlogToDto(Blog blog)
+// 		{
+// 			return new
+// 			{
+// 				Id = blog.Id,
+// 				Title = blog.Title,
+// 				Description = blog.Description,
+// 				CreatedAt = blog.CreatedAt,
+// 				Username = blog.User?.Username,
+// 				LikesCount = blog.Likes?.Count ?? 0,
+// 				DislikesCount = blog.Dislikes?.Count ?? 0,
+// 				CommentsCount = blog.Comments?.Count ?? 0,
+// 				Comments = blog.Comments?.Select(c => new {
+// 					c.Id,
+// 					c.Text,
+// 					UserName = c.User?.Username,
+// 					LikesCount = c.Likes?.Count ?? 0,
+// 					DislikesCount = c.Dislikes?.Count ?? 0
+// 				}),
+// 				ImageUrl = blog.ImagePath
+// 			};
+// 		}
+// 	}
+// }
 
-	}
-}
-*/
 
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -64,80 +92,86 @@ using BisleriumServer.Model;
 
 namespace BisleriumServer.Controllers
 {
-	[ApiController]
-	[Route("api/[controller]")]
-	public class GetBlogsController : ControllerBase
-	{
-		private readonly ApplicationDbContext _context;
+    [ApiController]
+    [Route("api/[controller]")]
+    public class GetBlogsController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
 
-		public GetBlogsController(ApplicationDbContext context)
-		{
-			_context = context;
-		}
+        public GetBlogsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
 
-		// Get random blogs
-		[HttpGet("random")]
-		public async Task<IActionResult> GetRandomBlogs()
-		{
-			var blogs = await _context.Blogs
-				.OrderBy(b => Guid.NewGuid())
-				.Take(10)
-				.Include(b => b.User)
-				.Include(b => b.Comments)
-				.ToListAsync();
+        // Get random blogs
+        [HttpGet("random")]
+        public async Task<IActionResult> GetRandomBlogs()
+        {
+            var blogs = await _context.Blogs
+                .OrderBy(b => Guid.NewGuid())
+                .Take(10)
+                .Include(b => b.User)
+                .Include(b => b.Comments)
+                .Include(b => b.Likes)
+                .Include(b => b.Dislikes)
+                .ToListAsync();
 
-			return Ok(blogs.Select(b => MapBlogToDto(b)));
-		}
+            return Ok(blogs.Select(b => MapBlogToDto(b)));
+        }
 
-		// Get blogs by recency
-		[HttpGet("recent")]
-		public async Task<IActionResult> GetRecentBlogs()
-		{
-			var blogs = await _context.Blogs
-				.OrderByDescending(b => b.CreatedAt)
-				.Take(10)
-				.Include(b => b.User)
-				.Include(b => b.Comments)
-				.ToListAsync();
+        // Get blogs by recency
+        [HttpGet("recent")]
+        public async Task<IActionResult> GetRecentBlogs()
+        {
+            var blogs = await _context.Blogs
+                .OrderByDescending(b => b.CreatedAt)
+                .Take(10)
+                .Include(b => b.User)
+                .Include(b => b.Comments)
+                .Include(b => b.Likes)
+                .Include(b => b.Dislikes)
+                .ToListAsync();
 
-			return Ok(blogs.Select(b => MapBlogToDto(b)));
-		}
+            return Ok(blogs.Select(b => MapBlogToDto(b)));
+        }
 
-		// Get blogs by popularity (likes and comments)
-		[HttpGet("popular")]
-		public async Task<IActionResult> GetPopularBlogs()
-		{
-			var blogs = await _context.Blogs
-				.Include(b => b.User)
-				.Include(b => b.Comments)
-				.OrderByDescending(b => b.Likes.Count + b.Comments.Count)
-				.Take(10)
-				.ToListAsync();
+        // Get blogs by popularity (likes and comments)
+        [HttpGet("popular")]
+        public async Task<IActionResult> GetPopularBlogs()
+        {
+            var blogs = await _context.Blogs
+                .Include(b => b.User)
+                .Include(b => b.Comments)
+                .Include(b => b.Likes)
+                .Include(b => b.Dislikes)
+                .OrderByDescending(b => b.Likes.Count + b.Comments.Count)
+                .Take(10)
+                .ToListAsync();
 
-			return Ok(blogs.Select(b => MapBlogToDto(b)));
-		}
+            return Ok(blogs.Select(b => MapBlogToDto(b)));
+        }
 
-		private dynamic MapBlogToDto(Blog blog)
-		{
-			return new
-			{
-				Id = blog.Id,
-				Title = blog.Title,
-				Description = blog.Description,
-				CreatedAt = blog.CreatedAt,
-				Username = blog.User?.Username,
-				LikesCount = blog.Likes?.Count ?? 0,
-				DislikesCount = blog.Dislikes?.Count ?? 0,
-				CommentsCount = blog.Comments?.Count ?? 0,
-				Comments = blog.Comments?.Select(c => new {
-					c.Id,
-					c.Text,
-					UserName = c.User?.Username,
-					LikesCount = c.Likes?.Count ?? 0,
-					DislikesCount = c.Dislikes?.Count ?? 0
-				}),
-				ImageUrl = blog.ImagePath
-			};
-		}
-	}
+        private dynamic MapBlogToDto(Blog blog)
+        {
+            return new
+            {
+                Id = blog.Id,
+                Title = blog.Title,
+                Description = blog.Description,
+                CreatedAt = blog.CreatedAt,
+                Username = blog.User?.Username,
+                LikesCount = blog.Likes?.Count() ?? 0,
+                DislikesCount = blog.Dislikes?.Count() ?? 0,
+                CommentsCount = blog.Comments?.Count() ?? 0,
+                Comments = blog.Comments?.Select(c => new {
+                    c.Id,
+                    c.Text,
+                    UserName = c.User?.Username,
+                    LikesCount = c.Likes?.Count() ?? 0,
+                    DislikesCount = c.Dislikes?.Count() ?? 0
+                }).ToList(),
+                ImageUrl = blog.ImagePath
+            };
+        }
+    }
 }
--- Controllers/LikeBlogController.cs
+++ Controllers/LikeBlogController.cs
@@ -1,62 +1,123 @@
-﻿using BisleriumServer.Data;
+﻿// using BisleriumServer.Data;
+// using BisleriumServer.Model;
+// using Microsoft.AspNetCore.Authorization;
+// using Microsoft.AspNetCore.Mvc;
+// using Microsoft.EntityFrameworkCore;
+
+
+// namespace BisleriumServer.Controllers
+// {
+// 	[Authorize]
+// 	[ApiController]
+// 	[Route("api/[controller]")]
+// 	public class LikeBlogController : Controller
+// 	{
+// 		private readonly ApplicationDbContext _context;
+
+// 		public LikeBlogController(ApplicationDbContext context)
+// 		{
+// 			_context = context;
+// 		}
+
+// 		// POST: api/LikeBlog
+// 		[HttpPost]
+// 		public async Task<IActionResult> ToggleLike(int blogId, int userId)
+// 		{
+
+// 			// Check if the blog exists
+// 			var blog = await _context.Blogs.FindAsync(blogId);
+// 			if (blog == null)
+// 			{
+// 				return NotFound();
+// 			}
+
+// 			// Check if the user has already liked the blog
+// 			var existingLike = await _context.Likes.FirstOrDefaultAsync(l => l.BlogId == blogId && l.UserId == userId);
+// 			if (existingLike != null)
+// 			{
+// 				// If a like exists, remove it and decrement the likesCount
+// 				_context.Likes.Remove(existingLike);
+// 				blog.LikesCount--;
+// 			}
+// 			else
+// 			{
+// 				// If no like exists, create a new one and increment the likesCount
+// 				var like = new Like
+// 				{
+// 					BlogId = blogId,
+// 					UserId = userId
+// 				};
+
+// 				_context.Likes.Add(like);
+// 				blog.LikesCount++;
+// 			}
+
+// 			await _context.SaveChangesAsync();
+
+// 			return Ok();
+// 		}
+
+
+// 	}
+// }
+
+using BisleriumServer.Data;
 using BisleriumServer.Model;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-
+using System.Security.Claims;
 
 namespace BisleriumServer.Controllers
 {
-	[Authorize]
-	[ApiController]
-	[Route("api/[controller]")]
-	public class LikeBlogController : Controller
-	{
-		private readonly ApplicationDbContext _context;
+    [Authorize]
+    [ApiController]
+    [Route("api/[controller]")]
+    public class LikeBlogController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
 
-		public LikeBlogController(ApplicationDbContext context)
-		{
-			_context = context;
-		}
+        public LikeBlogController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
 
-		// POST: api/LikeBlog
-		[HttpPost]
-		public async Task<IActionResult> ToggleLike(int blogId, int userId)
-		{
+        // POST: api/LikeBlog
+        [HttpPost]
+        public async Task<IActionResult> ToggleLike(int blogId)
+        {
+            int userId = int.Parse(User.FindFirst(ClaimTypes.NameIdentifier)?.Value);
 
-			// Check if the blog exists
-			var blog = await _context.Blogs.FindAsync(blogId);
-			if (blog == null)
-			{
-				return NotFound();
-			}
+            // Check if the blog exists
+            var blog = await _context.Blogs.FindAsync(blogId);
+            if (blog == null)
+            {
+                return NotFound("Blog not found.");
+            }
 
-			// Check if the user has already liked the blog
-			var existingLike = await _context.Likes.FirstOrDefaultAsync(l => l.BlogId == blogId && l.UserId == userId);
-			if (existingLike != null)
-			{
-				// If a like exists, remove it and decrement the likesCount
-				_context.Likes.Remove(existingLike);
-				blog.LikesCount--;
-			}
-			else
-			{
-				// If no like exists, create a new one and increment the likesCount
-				var like = new Like
-				{
-					BlogId = blogId,
-					UserId = userId
-				};
+            // Check if the user has already liked the blog
+            var existingLike = await _context.Likes.FirstOrDefaultAsync(l => l.BlogId == blogId && l.UserId == userId);
+            if (existingLike != null)
+            {
+                // If a like exists, remove it
+                _context.Likes.Remove(existingLike);
+            }
+            else
+            {
+                // If no like exists, create a new one
+                var like = new Like
+                {
+                    BlogId = blogId,
+                    UserId = userId
+                };
+                _context.Likes.Add(like);
+            }
 
-				_context.Likes.Add(like);
-				blog.LikesCount++;
-			}
+            await _context.SaveChangesAsync();
 
-			await _context.SaveChangesAsync();
-
-			return Ok();
-		}
-
-
-	}
+            // Return the current count of likes to ensure client-side consistency
+            var currentLikesCount = await _context.Likes.CountAsync(l => l.BlogId == blogId);
+            return Ok(new { LikesCount = currentLikesCount });
+        }
+    }
 }
